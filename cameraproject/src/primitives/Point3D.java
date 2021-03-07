5cc4a05ab9d1cec5a4b563fb4f1321c9f97518c5
AviR <AviR@10.0.0.32>
2021-03-07 16:02:30 +0200

    Vector works

--- cameraproject/src/primitives/Point3D.java
+++ cameraproject/src/primitives/Point3D.java
@@ -6,6 +6,43 @@ public class Point3D {
 	private Coordinate y;
 	private Coordinate z;
 	
+	public static Point3D ZERO = new Point3D(0,0,0); 
+	
+	public Point3D(Coordinate x, Coordinate y, Coordinate z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public Point3D(double x_value, double y_value, double z_value) {
+		super();
+		this.x = new Coordinate(x_value);
+		this.y = new Coordinate(y_value);
+		this.z = new Coordinate(z_value);
+	}
+	
+	public Coordinate getX() {
+		return x;
+	}
+	public Coordinate getY() {
+		return y;
+	}
+	public Coordinate getZ() {
+		return z;
+	}
+
+	public double getX_value() {
+		return x.coord;
+	}
+	public double getY_value() {
+		return y.coord;
+	}
+	public double getZ_value() {
+		return z.coord;
+	}
+
+	
 	public Point3D add(Vector v){
 		
 	}
--- cameraproject/src/primitives/Vector.java
+++ cameraproject/src/primitives/Vector.java
@@ -4,43 +4,92 @@ public class Vector {
 
 	private Point3D head;
 	
-	public Vector add(Vector vector){
+	public Vector(Point3D _head) {
+		super();
+		
+		if(this.head.equals(new Point3D(0,0,0)))
+			throw new IllegalArgumentException ("Can't construct an empty vector");
+		
+		this.head = _head;
+	}
+	
+	public Vector(double x,double y,double z) {
+		Point3D point = new Point3D(x,y,z);
+		if(point.equals(Point3D.ZERO))
+			throw new IllegalArgumentException ("Can't construct an empty vector");
+		this.head = point;
 		
 	}
 	
-	public Vector subtract(Vector vector){
+	public Vector(Coordinate x,Coordinate y,Coordinate z) {
+		Point3D point = new Point3D(x,y,z);
+		if(point.equals(Point3D.ZERO))
+			throw new IllegalArgumentException ("Can't construct an empty vector");
+		this.head = point;
+	}
+	
+	public Point3D getHead() {
+		return head;
+	}
+
+
+
+	public Vector add(Vector vector){
 		
+		Point3D addidtionCoordinates = new Point3D(this.head.getX_value()+vector.getHead().getX_value(), this.head.getY_value()+vector.getHead().getY_value(),this.head.getZ_value()+vector.getHead().getZ_value());
+		
+		return new Vector(addidtionCoordinates);
+	}
+	
+
+	public Vector subtract(Vector vector){
+		Point3D subtractionCoordinates = new Point3D(this.head.getX_value()-vector.getHead().getX_value(), this.head.getY_value()-vector.getHead().getY_value(),this.head.getZ_value()-vector.getHead().getZ_value());
+		
+		return new Vector(subtractionCoordinates);
 	}
 	
 	public Vector scale(double scalar){
+		Point3D scaledCoordinates = new Point3D(this.head.getX_value()*scalar, this.head.getY_value()*scalar,this.head.getZ_value()*scalar);
 		
+		return new Vector(scaledCoordinates);
 	}
 	
 	public Vector crossProduct(Vector vector){
-		
+		Point3D crossproduct = new Point3D(this.head.getY_value()*vector.getHead().getZ_value()-this.head.getZ_value()*vector.getHead().getY_value(),
+				-(this.head.getX_value()*vector.getHead().getZ_value()-this.head.getZ_value()*vector.getHead().getX_value()),
+				this.head.getY_value()*vector.getHead().getX_value()-this.head.getX_value()*vector.getHead().getY_value()
+				);
+		return new Vector(crossproduct);
 	}
 	
 	public double dotProduct(Vector vector){
-		
+		return this.head.getX_value()*vector.getHead().getX_value()+ this.head.getY_value()*vector.getHead().getY_value()+this.head.getZ_value()*vector.getHead().getZ_value();
 	}
 	
 	public double lengthSquared(){
-		
+		return this.head.getX_value()*this.head.getX_value()+this.head.getY_value()*this.head.getY_value()+this.head.getZ_value()*this.head.getZ_value();
 	}
 	
 	public double length(){
-		
+		return Math.sqrt(this.lengthSquared());
 	}
 	
 	public Vector normalize(){
-		
+
+		this.head =this.scale(1/this.length()).getHead();
+		return this;
 	}
 	public Vector normalized(){
-		
+		return this.scale(1/this.length());
 	}
 	
 	@Override
-	public boolean equals(Object object){
-	
+	public boolean equals(Object obj){
+	      if (this == obj) return true;
+	      if (obj == null) return false;
+	      if (!(obj instanceof Vector)) return false;
+	      Vector other = (Vector)obj;
+	      return this.head.equals(other.head);
+
 	}
 }
