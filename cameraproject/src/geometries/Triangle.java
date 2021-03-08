f04a43cabeb9d40dea347fdc6f31215762951f03
AviR <AviR@10.0.0.32>
2021-03-08 12:26:04 +0200

    tostring

--- cameraproject/src/geometries/Triangle.java
+++ cameraproject/src/geometries/Triangle.java
@@ -6,10 +6,10 @@ public class Triangle extends Polygon {
 	public Triangle(Point3D point1,Point3D point2,Point3D point3) { // Ctor for the triangle: puts 3 (and only 3) points into the Polygon base.
 		super(point1,point2,point3);
 	}
-
+/*
 	@Override
 	public String toString() {// the tostring of polygon returns the entire list. so there is no do anything different here.
 		return super.toString();
 	}
-	
+	*/
 }
