1f9e826084bd86993f9b53344b80802a10148043
Eliyahu-r <AviR@10.0.0.32>
2021-03-21 10:25:50 +0200

    plane normal test

--- cameraproject/src/unittests/geometries/PlaneTest.java
+++ cameraproject/src/unittests/geometries/PlaneTest.java
@@ -4,11 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import geometries.Plane;
+import primitives.Point3D;
+import primitives.Vector;
+
 public class PlaneTest {
 
 	@Test
-	public void testGetNormalPoint3D() {
-		fail("Not yet implemented");
+	public void testGetNormalPoint3D() { // not sure about this ver
+		Plane p = new Plane(new Point3D(1,1,1),new Vector(1,2,3));
+		assertEquals("Mistake found with GetNormal",p.getNormal(new Point3D(7,-2,1)),new Vector(1,2,3).normalize());
 	}
 
 }
