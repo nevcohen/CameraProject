1df71d2957a9a78511f7ec2aa6ca283e6c87f37a
Nevo Cohen <nevcohen@g.jct.ac.il>
2021-03-21 10:11:03 +0200

    Tube tests

--- cameraproject/src/unittests/geometries/TubeTest.java
+++ cameraproject/src/unittests/geometries/TubeTest.java
@@ -17,6 +17,8 @@ public class TubeTest {
 		
 		// ============ Equivalence Partitions Tests ==============	
 		assertEquals("",tube.getNormal(new Point3D(0,2,0)),new Vector(0,1,0));
+		
+		assertEquals("",tube.getNormal(new Point3D(0,2,2)),new Vector(0,1,0));//-------
 	}
 
 }
