3b4afa04a4b3e1f54684f0fe67fd7a8ca6856301
Nevo Cohen <nevcohen@g.jct.ac.il>
2021-04-25 14:28:04 +0300

    start IntegrationTest

---
+++ cameraproject/src/unittests/elements/IntegrationTest.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package unittests.elements;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class IntegrationTest {
+
+	/**
+	 * Test method for
+	 * {@link elements.Camera#constructRayThroughPixel(int, int, int, int)}.
+	 */
+	@Test
+	public void testSphere() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link elements.Camera#constructRayThroughPixel(int, int, int, int)}.
+	 */
+	@Test
+	public void testPlane() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link elements.Camera#constructRayThroughPixel(int, int, int, int)}.
+	 */
+	@Test
+	public void testTriangle() {
+		fail("Not yet implemented");
+	}
+
+}
