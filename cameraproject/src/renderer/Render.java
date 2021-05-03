85e4efbd13c09fe73e75399eb5d5d107914afbef
Nevo Cohen <nevcohen@g.jct.ac.il>
2021-05-03 15:39:30 +0300

    javadoc2 - finish exe5

--- cameraproject/src/renderer/Render.java
+++ cameraproject/src/renderer/Render.java
@@ -8,32 +8,32 @@ import primitives.Ray;
 import scene.Scene;
 
 /**
- * 
+ * A class to render the picture, and create the image.
  */
 public class Render {
 
 	/**
-	 * 
+	 * A variable used to export the image in the desired format
 	 */
 	ImageWriter imageWriter;
 	/**
-	 * 
+	 * Details of the scene used for the picture
 	 */
 	Scene scene;
 	/**
-	 * 
+	 * Details of the location of the camera
 	 */
 	Camera camera;
 	/**
-	 * 
+	 * Details of the tracer
 	 */
 	RayTracerBase rayTracerBase;
 
 	/**
+	 * A function designed to change the setting of the image writing process
 	 * 
-	 * 
-	 * @param imageWriter
-	 * @return
+	 * @param imageWriter – the new writer we will base the render on
+	 * @return the render including the new writer
 	 */
 	public Render setImageWriter(ImageWriter imageWriter) {
 		this.imageWriter = imageWriter;
@@ -41,10 +41,10 @@ public class Render {
 	}
 
 	/**
+	 * A method to change the scene behind the desired picture.
 	 * 
-	 * 
-	 * @param scene
-	 * @return
+	 * @param scene – the new scene used for the picture
+	 * @return the new render, including the updated scene.
 	 */
 	public Render setScene(Scene scene) {
 		this.scene = scene;
@@ -52,10 +52,11 @@ public class Render {
 	}
 
 	/**
+	 * A method to change the camera, and/or its’ location, behind the desired
+	 * picture.
 	 * 
-	 * 
-	 * @param camera
-	 * @return
+	 * @param camera – the new camera/camera angle used for the picture
+	 * @return the new render, including the updated camera position.
 	 */
 	public Render setCamera(Camera camera) {
 		this.camera = camera;
@@ -63,10 +64,10 @@ public class Render {
 	}
 
 	/**
+	 * A function to update the ray tracer.
 	 * 
-	 * 
-	 * @param rayTracerBase
-	 * @return
+	 * @param rayTracerBase – the new ray tracer.
+	 * @return the new renderer, including the updated ray tracer.
 	 */
 	public Render setRayTracer(RayTracerBase rayTracerBase) {
 		this.rayTracerBase = rayTracerBase;
@@ -74,7 +75,7 @@ public class Render {
 	}
 
 	/**
-	 * 
+	 * A function used to take the image information, and render it into a picture.
 	 */
 	public void renderImage() {
 		if (imageWriter == null || scene == null || camera == null || rayTracerBase == null)
@@ -94,10 +95,10 @@ public class Render {
 	}
 
 	/**
+	 * A function to prepare the base of the picture, a.k.a, the grid.
 	 * 
-	 * 
-	 * @param interval
-	 * @param color
+	 * @param interval – the number used to set the size of the grid.
+	 * @param color    – the color we want to print the grid in.
 	 */
 	public void printGrid(int interval, Color color) {
 		if (imageWriter == null)
@@ -110,7 +111,7 @@ public class Render {
 	}
 
 	/**
-	 * 
+	 * A function that uses the imageWriter to generate the image
 	 */
 	public void writeToImage() {
 		if (imageWriter == null)
--- cameraproject/src/scene/Scene.java
+++ cameraproject/src/scene/Scene.java
@@ -5,30 +5,31 @@ import geometries.Geometries;
 import primitives.Color;
 
 /**
- *  
+ * Scene class: a class to represent a scene which will be the base for the
+ * picture
  */
 public class Scene {
 	/**
-	 * 
+	 * The scene needs a name
 	 */
 	public String name;
 	/**
-	 * 
+	 * The base is black
 	 */
 	public Color background = Color.BLACK;
 	/**
-	 * 
+	 * Definition of the ambient light for the background
 	 */
 	public AmbientLight ambientLight = new AmbientLight(background, 0);
 	/**
-	 * 
+	 * All the geometric shapes that exist in the scene
 	 */
 	public Geometries geometries;
 
 	/**
+	 * Constructor gets the name of the scene
 	 * 
-	 * 
-	 * @param name
+	 * @param name – string. In order to name the scene.
 	 */
 	public Scene(String name) {
 		this.name = name;
@@ -36,10 +37,10 @@ public class Scene {
 	}
 
 	/**
+	 * A function to have a background other than the default of black
 	 * 
-	 * 
-	 * @param background
-	 * @return
+	 * @param background – the new color needed to color the background
+	 * @return the scene itself – with an updated background.
 	 */
 	public Scene setBackground(Color background) {
 		this.background = background;
@@ -47,10 +48,10 @@ public class Scene {
 	}
 
 	/**
+	 * A function to change the ambientLight.
 	 * 
-	 * 
-	 * @param ambientLight
-	 * @return
+	 * @param ambientLight setting new parameters for the ambient light
+	 * @return the scene, but with different ambient lighting.
 	 */
 	public Scene setAmbientLight(AmbientLight ambientLight) {
 		this.ambientLight = ambientLight;
@@ -58,10 +59,10 @@ public class Scene {
 	}
 
 	/**
+	 * A function to change the geometries in the scene.
 	 * 
-	 * 
-	 * @param geometries
-	 * @return
+	 * @param geometries - setting new shapes for the background
+	 * @return the scene, but with different shapes.
 	 */
 	public Scene setGeometries(Geometries geometries) {
 		this.geometries = geometries;
